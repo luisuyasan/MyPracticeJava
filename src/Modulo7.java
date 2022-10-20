2b483a41a0d9c66844869a70a5acd4645c972204
luis.uyasan <uyasanluis@gmail.com>
2022-10-20 18:10:00 -0500

    changes 4 full

--- src/Modulo7.java
+++ src/Modulo7.java
@@ -1,10 +1,15 @@
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
-public class Modulo7
-{
-    public static void main(String[] args)
-    {
+public class Modulo7 {
+    public static void main(String[] args) {
+
+        ControlDate();
+        ControlCalendar();
+    }
+
+    public static void ControlDate() {
         Date date = new Date();
         SimpleDateFormat formatDate = new SimpleDateFormat("EEEE dd 'de' MMMM 'del' YYYY");
         System.out.println(formatDate.format(date));
@@ -12,7 +17,23 @@ public class Modulo7
         Date date2 = new Date();
         long endTime = date2.getTime() - date.getTime();
         System.out.println("endTime = " + endTime);
+    }
 
+    public static void ControlCalendar() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1989,9,30);
+        Date date = calendar.getTime();
+        System.out.println("date = " + date);
+
+        calendar.set(Calendar.YEAR, 1996);
+        calendar.set(Calendar.MONTH,11);
+        calendar.set(Calendar.DAY_OF_MONTH, 11);
+        calendar.set(Calendar.HOUR_OF_DAY, 8);
+        calendar.set(Calendar.MINUTE, 45);
+        calendar.set(Calendar.SECOND, 28);
+        Date date2 = calendar.getTime();
+        System.out.println("date2 = " + date2);
 
     }
 }
+
