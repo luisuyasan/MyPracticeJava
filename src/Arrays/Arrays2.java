e176e3fe46d1782f2b32ce5c47dbf83b66a4e40e
luis.uyasan <uyasanluis@gmail.com>
2022-10-25 17:49:35 -0500

    changes 25/10/22 17:50 full

---
+++ src/Arrays/Arrays2.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Arrays2
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input quantity numbers do you want");
+        Integer lengthArray = scanner.nextInt();
+        Integer[] array = new Integer[lengthArray];
+        fellingUpArray(array);
+    }
+
+
+    //method filling up array with data from user
+    public static void fellingUpArray(Integer[] array){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input numbers");
+        for (int i = 0; i < array.length;i++){
+            array[i] = scanner.nextInt();
+        }
+        int max = 0;
+        for (int i = 0;i < array.length; i++){
+            max = (array[max] > array[i])? max : i;
+        }
+        System.out.println("max = " + array[max]);
+    }
+
+
+}
